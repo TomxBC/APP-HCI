400f26389ac38b607bd3e51eacdff631da4133af
tfeeney0 <tfeeney@itba.edu.ar>
2024-06-15 15:54:56 -0300

    Update button spacing

--- app/src/main/java/com/example/turnsmart_hci/devices/button.kt
+++ app/src/main/java/com/example/turnsmart_hci/devices/button.kt
@@ -1,7 +1,9 @@
 package com.example.turnsmart_hci.devices
 
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
@@ -12,10 +14,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.turnsmart_hci.R
 import com.example.turnsmart_hci.ui.theme.darkText
+import com.example.turnsmart_hci.ui.theme.montserratFontFamily
 import com.example.turnsmart_hci.ui.theme.pale_blue
 
 @Composable
@@ -39,13 +44,18 @@ fun CustomButton(
         Icon(
             painter = painterResource(icon),
             contentDescription = null,
-            modifier = Modifier.padding(start = 8.dp).size(24.dp),
+            modifier = Modifier.padding(start = 0.dp).size(24.dp),
             tint = Color.Black
         )
+        Spacer(modifier = Modifier.width(60.dp))
         Text(
             text = label,
             color = textColor,
+            fontSize = 18.sp,
+            fontFamily = montserratFontFamily,
+            fontWeight = FontWeight.Medium
         )
+        Spacer(modifier = Modifier.width(80.dp))
         IconButton(
             onClick = { }
         ){
