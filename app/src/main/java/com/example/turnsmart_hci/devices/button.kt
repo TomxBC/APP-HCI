1d3b072a9dcb495e750490c082f0efcb2dc53a5b
TomxBC <tobecerra@itba.edu.ar>
2024-06-18 11:55:32 -0300

    Revert some changes
    Co-Authored-By: AgusAlonso1 <112195368+AgusAlonso1@users.noreply.github.com>
    Co-Authored-By: tfeeney0 <131275532+tfeeney0@users.noreply.github.com>
    Co-Authored-By: Magdalena Cullen <106096956+mcullen212@users.noreply.github.com>

--- app/src/main/java/com/example/turnsmart_hci/devices/button.kt
+++ app/src/main/java/com/example/turnsmart_hci/devices/button.kt
@@ -1,6 +1,5 @@
 package com.example.turnsmart_hci.devices
 
-import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -20,16 +19,16 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.turnsmart_hci.R
+import com.example.turnsmart_hci.ui.theme.darkText
 import com.example.turnsmart_hci.ui.theme.montserratFontFamily
-import com.example.turnsmart_hci.ui.theme.ThemeColors
+import com.example.turnsmart_hci.ui.theme.pale_blue
 
 @Composable
 fun DeviceButton(
-    @StringRes label: Int,
-    enabled: Boolean = true,
+    label: String,
     onClick: () -> Unit,
     backgroundColor: Color,
-    textColor: Color = ThemeColors.DARK_TEXT.color,
+    textColor: Color = darkText,
     icon: Int,
     modifier: Modifier = Modifier
 ) {
@@ -41,19 +40,16 @@ fun DeviceButton(
         modifier = modifier
             .padding(8.dp)
             .size(width = 300.dp, height = 50.dp),
-        enabled = enabled
     ) {
         Icon(
             painter = painterResource(icon),
             contentDescription = null,
-            modifier = Modifier
-                .padding(start = 0.dp)
-                .size(24.dp),
+            modifier = Modifier.padding(start = 0.dp).size(24.dp),
             tint = Color.Black
         )
         Spacer(modifier = Modifier.width(10.dp))
         Text(
-            text = stringResource(id = label),
+            text = label,
             color = textColor,
             fontSize = 18.sp,
             fontFamily = montserratFontFamily,
@@ -77,6 +73,6 @@ fun DeviceButton(
 @Composable
 fun DeviceButtonPreview() {
     DeviceButton(
-        label = R.string.lights, onClick = {},
-        icon = R.drawable.lights, backgroundColor = ThemeColors.PALE_BLUE.color)
+        label = stringResource(id = R.string.lights), onClick = {},
+        icon = R.drawable.lights, backgroundColor = pale_blue)
 }
