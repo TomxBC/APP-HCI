34545557df129b36f3f8fdd3d9ba858e0f51827d
Magdalena Cullen <106096956+mcullen212@users.noreply.github.com>
2024-06-18 12:50:41 -0300

    Theme applied

--- app/src/main/java/com/example/turnsmart_hci/navBars/Toolbar.kt
+++ app/src/main/java/com/example/turnsmart_hci/navBars/Toolbar.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
@@ -54,8 +55,8 @@ fun TurnSmartToolbar(navController: NavController, currentScreenTitle: MutableSt
             }
         },
         colors = TopAppBarDefaults.topAppBarColors(
-            containerColor = Color.Black,
-            titleContentColor = lightText
+            containerColor = MaterialTheme.colorScheme.secondary,
+            titleContentColor = MaterialTheme.colorScheme.onSecondary
         ),
         actions = {
             if (navController.currentDestination?.route != Screens.Settings.route) {
@@ -75,7 +76,7 @@ fun TurnSmartToolbar(navController: NavController, currentScreenTitle: MutableSt
                     Icon(
                         painter = painterResource(id = R.drawable.settings),
                         contentDescription = stringResource(id = R.string.settings_label),
-                        tint = lightText
+                        tint = MaterialTheme.colorScheme.onSecondary
                     )
                 }
             }
--- app/src/main/java/com/example/turnsmart_hci/ui/theme/Color.kt
+++ app/src/main/java/com/example/turnsmart_hci/ui/theme/Color.kt
@@ -15,19 +15,10 @@ val pale_blue = Color(0xFFA3CEFF)
 val pale_green = Color(0xFFD3FCD5)
 val pale_red = Color(0xFFFCCDCD)
 val pale_yellow = Color(0xFFF4F9BA)
+
+val dark_purple = Color(0xFF6650a4)
 val pale_purple = Color(0xFFDBC3F7)
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
-
-//navbar
-val selected_navbar = Color(0xFF6650a4)
-val background_navbar = Color(0xFFD0BCFF)
 enum class ThemeColors(val color: Color) {
     DARK_BOTTOM_APP_BAR(Color(0xFF4D4B4B)),
     LIGHT_BOTTOM_APP_BAR(Color(0xFFEBE5E5)),
