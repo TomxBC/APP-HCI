dffabb8ba69c20c8559db176c39672788a6b43f2
tfeeney0 <tfeeney@itba.edu.ar>
2024-06-23 00:40:59 -0300

    Speakers and Blinds now translating

--- app/src/main/java/com/example/turnsmart_hci/devices/BlindsButton.kt
+++ app/src/main/java/com/example/turnsmart_hci/devices/BlindsButton.kt
@@ -31,6 +31,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -188,7 +189,7 @@ fun BlindsScreen(
                 modifier = Modifier.fillMaxWidth().padding(10.dp)
             ) {
                 Text(
-                    text = if(isOpen) "OPEN" else "CLOSE",
+                    text = if(isOpen) stringResource(id = R.string.open_blind) else stringResource(id = R.string.close),
                     color = textColor,
                     fontSize = 16.sp,
                     fontFamily = montserratFontFamily,
@@ -205,7 +206,7 @@ fun BlindsScreen(
             }
             Spacer(modifier = Modifier.height(16.dp).padding(10.dp))
             Text(
-                text = "Blind position:",
+                text = stringResource(id = R.string.blind_position) ,
                 color = textColor,
                 fontSize = 16.sp,
                 fontFamily = montserratFontFamily,
