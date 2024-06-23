819b06b97a49027e9d1c06dde8b5a77823cf1277
tfeeney0 <tfeeney@itba.edu.ar>
2024-06-23 14:05:32 -0300

    Update SettingsScreen.kt

--- app/src/main/java/com/example/turnsmart_hci/screens/SettingsScreen.kt
+++ app/src/main/java/com/example/turnsmart_hci/screens/SettingsScreen.kt
@@ -1,24 +1,12 @@
 package com.example.turnsmart_hci.screens
 
-import android.Manifest
-import android.content.pm.PackageManager
-import android.os.Build
-import androidx.activity.result.contract.ActivityResultContracts
+
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.RadioButton
-import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.core.content.ContextCompat
 import com.example.turnsmart_hci.notifications.NotificationViewModel
 import com.example.turnsmart_hci.ui.theme.TurnSmartTheme
 import com.example.turnsmart_hci.ui.theme.montserratFontFamily
